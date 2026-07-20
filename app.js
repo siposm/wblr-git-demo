e54b96e4e560659532bba54cec2569ca49e6ecab
Sipos Miklos <commit@siposm.hu>
2026-07-20 19:15:31 +0200

    app logic created

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-class Calculator {
-	add(a, b) {
-		return a + b
-	}
+let calc = new Calculator()
+
+function addition() {
+	let paramA = Number(document.querySelector("#param-a").value)
+	let paramB = Number(document.querySelector("#param-b").value)
+
+	let result = calc.add(paramA, paramB)
+
+	console.log(result)
 }
