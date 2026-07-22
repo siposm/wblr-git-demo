16153996b6cbb0157dc4b5f710adb74c21954abf
Blanka <blanka@hello.com>
2026-07-22 18:42:12 +0200

    division by zero handled

--- calc.js
+++ calc.js
@@ -1,14 +1,22 @@
 class Calculator {
+
 	add(a, b) {
 		return a + b
 	}
+
 	sub(a, b) {
 		return a - b
 	}
+
 	mul(a, b){
 		return a * b
 	}
+
 	div(a, b) {
-		return a / b
+		if (b == 0) {
+			throw new Error("HIBA: Nullával való osztási kísérlet.")
+		} else {
+			return a / b
+		}
 	}
 }
